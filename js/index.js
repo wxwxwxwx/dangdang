5d120076b835b315f82218a6788b8b0b7e872b20
王昕 <97677613@qq.com>
2016-06-13 23:56:14 +0800

    ok

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    // 如果需要分页器
+    loop:true,
+    pagination: '.swiper-pagination',
+    autoplay : 3000,
+    speed:200,
+  })
+
+
+})
